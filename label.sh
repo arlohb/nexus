f3776d4a445ac3c738cd3917034b602d593a753e
arlohb <arlo@urwick.co.uk>
2025-05-09 10:12:47 +0100

    Added homeassistant

--- label.sh
+++ label.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 # transcode
-kubectl label node datasphere transcode=false
-kubectl label node neptune transcode=false
-kubectl label node pluto transcode=false
-kubectl label node arlo-laptop1 transcode=true
+kubectl label node datasphere transcode=false --overwrite
+kubectl label node neptune transcode=false --overwrite
+kubectl label node pluto transcode=false --overwrite
+kubectl label node arlo-laptop1 transcode=true --overwrite
 
 # network
-kubectl label node datasphere network=fast
-kubectl label node neptune network=fast
-kubectl label node pluto network=fast
-kubectl label node arlo-laptop1 network=slow
+kubectl label node datasphere network=fast --overwrite
+kubectl label node neptune network=fast --overwrite
+kubectl label node pluto network=fast --overwrite
+kubectl label node arlo-laptop1 network=slow --overwrite
+
+# zigbee
+kubectl label node datasphere zigbee=false --overwrite
+kubectl label node neptune zigbee=false --overwrite
+kubectl label node pluto zigbee=true --overwrite
+kubectl label node arlo-laptop1 zigbee=false --overwrite
 
